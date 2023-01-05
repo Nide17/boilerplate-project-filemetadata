f05918b73c05e3c40b6ab259f94bd764e1421ebc
Nide17 <nidedrogba@gmail.com>
2023-01-05 05:29:49 +0200

    dep:

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-var express = require('express');
-var cors = require('cors');
 require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const multer = require('multer')
 
 var app = express();
 
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
 app.use('/public', express.static(process.cwd() + '/public'));
+
+// connect to the database
+mongoose.set('strictQuery', false)
+mongoose.connect('mongodb+srv://parmenide:jesus123@fccmongoose.srblnut.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// create a schema
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: Number
+})
+
+// create a model
+const Url = mongoose.model('Url', urlSchema)
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
